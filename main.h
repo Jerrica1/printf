a256e303e05e757076a72bd2997e96445a511123
Gold Israel <techiegold101@gmail.com>
2023-01-25 12:21:54 -0800

    modified main.h

--- main.h
+++ main.h
@@ -37,7 +37,7 @@ struct fmt
   * @fmt: The format.
   * @fmt_t - The function associated.
   */
-type struct fmt fmt_t;
+typedef fmt fmt_t;
 
 int _printf(const char *fromat, ...);
 int handle_print(const char *fmt, int *i, va_list list, char buffer[],
@@ -90,7 +90,7 @@ int print_rot13string(va_list types, char buffer[], int flags,
 /* width handler */
 int handle_write_char(char c, char buffer[], int flags,
 		int width, int precision, int size);
-int write_number(int is_positive, int ind, char buffer[], 
+int write_number(int is_positive, int ind, char buffer[],
 		int flags, int width, int precision, int size);
 int write_num(int ind, char bff[], int flags, int width, int precision,
 		int length, char padd, char extra_c);
