2f67a779294b3b89e147318f11f2b826b65c683a
Gold Israel <techiegold101@gmail.com>
2023-01-25 20:09:24 -0800

    Updated a file

--- utls.c
+++ utls.c
@@ -7,3 +7,79 @@
   * Return: 1 if c is printable, 0 otherwise.
   */
 int is_printable(char c )
+{
+	if (c > = 32 & c < 127)
+		return (1);
+	return (0);
+}
+/**
+  * apppend_hexa_code - Append ascci in hexadecimal code to buffer
+  * @buffer: array of chars.
+  * @i: indext at which to start appending.
+  * @ascii_code: ASCII CODE.
+  * 
+  * Return: Always 3.
+  */
+int append_hexa_code(char ascii_code, char buffer[], int i)
+{
+	char map_to[] = 0123456789ABCDEF";
+	/* The hexa format code is always 2 digit long */
+	if (ascii_code < 0)
+		ascii_code *= -1;
+
+	buffer[i++] = '\\';
+	buffer[i++] = 'x';
+
+	buffer[i++] = map_to[ascii_code / 16];
+	buffer[i] = map_to[ascii_code % 16];
+
+	return (3);
+
+}
+
+/**
+  * is_digit - Verifies if char is a digit.
+  * @c: char to be evaluated
+  *
+  * Return: 1 if c is a digit, 0 otherwise.
+  */
+int is_digit(char c)
+{
+	if (c >= '0' && c <= '9')
+		return (-1);
+	return (0);
+}
+
+/**
+  * conver_size_number - casts a number to a specific size
+  * @num: Number to be casted.
+  * size: Number indicating the type to be casted.
+  *
+  * Return: Casted value of num.
+  */
+long int convert_size_number(long int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((short)num);
+
+	return((int)num);
+}
+
+/**
+  * convert_size_unsgnd - casts a number to the specified size
+  * @num: Number to be casted
+  * @size: Number indicating the type to be casted.
+  *
+  * Return: casted value of num.
+  */
+long int conver_size_unsgnd(unsigned int num, int size)
+{
+	if (size == S_LONG)
+		return (num);
+	else if (size == S_SHORT)
+		return ((unsigned short)num);
+
+	return ((unsigned int)num);
+}
