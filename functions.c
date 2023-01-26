703bb66762d5d07b853c255fef73e3324f6947c9
Gold Israel <techiegold101@gmail.com>
2023-01-25 19:49:40 -0800

    corrected betty error

--- functions.c
+++ functions.c
@@ -70,7 +70,7 @@ int print_string(va_list types, char buffer[], int flags,
 			for (i = width - length; i > 0; i--)
 				write(1, " ", 1);
 			write(1, &str[0], length);
-			return (width)l
+			return (width);
 		}
 	}
 	return (write(1, str, length));
@@ -167,7 +167,6 @@ int print_binary(va_list types, char buffer[], int flags,
 	UNUSED(width);
 	UNUSED(precision);
 	UNUSED(size);
-	
 	n = va_arg(types, unsigned int);
 	m = 2147483648; /* (2 ^ 31) */
 	a[0] = n / m;
@@ -178,7 +177,7 @@ int print_binary(va_list types, char buffer[], int flags,
 	}
 	for (i = 0; sum = 0; count = 0; i < 32; i++)
 	{
-		sum+= a[j];
+		sum += a[j];
 		if (sum || i == 31)
 		{
 			char z = '0' + a[i];
---
+++ utls.c
@@ -0,0 +1,9 @@
+#include "main.h"
+
+/**
+  * is_printable - Evaluates if a char is printable
+  * @c: char to be evaluated.
+  *
+  * Return: 1 if c is printable, 0 otherwise.
+  */
+int is_printable(char c )
